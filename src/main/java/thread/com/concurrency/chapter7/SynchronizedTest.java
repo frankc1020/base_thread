e0c6b6121676abb8fd6119847e0e81979c82a5b1
gzc1020 <gucong19961020@163.com>
2020-08-14 09:57:22 +0800

    约瑟夫环问题代码编写，以及解决思路

---
+++ src/main/java/thread/com/concurrency/chapter7/SynchronizedTest.java
@@ -0,0 +1,33 @@
+package thread.com.concurrency.chapter7;
+
+/**
+ * @author admin
+ * @title: SynchronizedTest
+ * @projectName base_thread
+ * @description: TODO
+ * @date 2020/8/12 16:43
+ */
+public class SynchronizedTest {
+
+    private final static  Object LOCK = new Object();
+
+    public static void main(String[] args) {
+        Runnable runnable = () ->{
+            synchronized (LOCK){
+                try {
+                    Thread.sleep(100_000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable);
+        Thread t3 = new Thread(runnable);
+        t1.start();
+        t2.start();
+        t3.start();
+
+    }
+}
